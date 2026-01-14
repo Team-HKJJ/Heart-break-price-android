0b1c5e87715d0b9bd160134c3a14f026293f3a65
Neouul <fluctu0q0@gmail.com>
2026-01-14 19:25:49 +0900

    #25 feat(search): SearchViewModel 의존성 주입 추가

--- app/src/main/java/com/hkjj/heartbreakprice/core/di/ViewModelModule.kt
+++ app/src/main/java/com/hkjj/heartbreakprice/core/di/ViewModelModule.kt
@@ -1,8 +1,13 @@
 package com.hkjj.heartbreakprice.core.di
 
+import com.hkjj.heartbreakprice.presentation.screen.search.SearchViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-
+    viewModel {
+        SearchViewModel(
+            getSearchedProductUseCase = get(),
+        )
+    }
 }
