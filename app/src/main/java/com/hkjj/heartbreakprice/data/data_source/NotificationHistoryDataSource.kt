26fc9f81bf7004cffb6cf990e9892e383d5bedca
hhp227 <hong227@naver.com>
2026-01-15 18:42:37 +0900

    #55 feat(notification): 알림 내역 데이터 소스 구현

---
+++ app/src/main/java/com/hkjj/heartbreakprice/data/data_source/NotificationHistoryDataSource.kt
@@ -0,0 +1,7 @@
+package com.hkjj.heartbreakprice.data.data_source
+
+import com.hkjj.heartbreakprice.domain.model.Notification
+
+interface NotificationHistoryDataSource {
+    suspend fun getAllNotificationHistories(): List<Notification>
+}
